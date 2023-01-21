f095979c453dfcb320874537c5b8e3ac8db9b540
Arthur Fernandez <arthur@snooget.com>
2023-01-21 14:52:40 -0800

    added a function to generate random winning number

--- src/script.js
+++ src/script.js
@@ -3,3 +3,9 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+
+//The first step for the number guesser game is to create a number generator to generate a random number between 1 - 9. This number will be the target number that 
+//-the computer and player must guess closest to in order to win.
+const generateTarget = () => {
+    
+};
