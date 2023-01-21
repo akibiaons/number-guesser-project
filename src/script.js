9c4e7b8e4654b707b466400001e5c3b924318e25
Arthur Fernandez <arthur@snooget.com>
2023-01-21 14:58:46 -0800

    added Math.floor, and Math.random to help generate a number between 1 - 9 for a winning target number for the mini-game

--- src/script.js
+++ src/script.js
@@ -7,5 +7,5 @@ let currentRoundNumber = 1;
 //The first step for the number guesser game is to create a number generator to generate a random number between 1 - 9. This number will be the target number that 
 //-the computer and player must guess closest to in order to win.
 const generateTarget = () => {
-    
+  return Math.floor(Math.random()*10);
 };
