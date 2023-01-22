465fc3314591c0e7f4d323118015115dd8567705
Arthur Fernandez <arthur@snooget.com>
2023-01-21 19:49:38 -0800

    added the last function to advance the round

--- src/script.js
+++ src/script.js
@@ -27,6 +27,11 @@ const updateScore = (winner) => {
         computerScore++;
     }
 };
+//Below will be the function that advances the round...
+const advanceRound = () => {
+    currentRoundNumber++;
+};
+// Below will be the code for advancing the round.
 // Write your code below:
 //The first step for the number guesser game is to create a number generator to generate a random number between 1 - 9. This number will be the target number that 
 //-the computer and player must guess closest to in order to win.
