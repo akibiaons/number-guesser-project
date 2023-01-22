229e1a847d32963b514301ee49a0cdce501f1a8e
Arthur Fernandez <arthur@snooget.com>
2023-01-21 16:05:51 -0800

    added working function to generate absolute value, will be adding a return statement that says who the winner is

--- src/script.js
+++ src/script.js
@@ -14,9 +14,15 @@ const generateTarget = () => {
 In the function titled generateTarget, we use Math.floor for a random number between 1 - 0 to be generated, then multiply thr number by 10 and math random..
 This random number generator is called with a console.log statement to call upon a random number
 */
-
+//================================================================================================================================================================================================
 /* 
 Below for step 2 of the JavaScript for the guessing game, we will now add a function that can determine which player will
 win (computer or human) based off of the absolute value of their response to the target number...the function also must return 
 the result of the round, win or lose based on the humans side.
 */
+const compareGuesses = (human, computer, target) => {
+    console.log(Math.abs(target - human));
+    console.log(Math.abs(target - computer));
+};
+
+compareGuesses(6, 1, generateTarget());
